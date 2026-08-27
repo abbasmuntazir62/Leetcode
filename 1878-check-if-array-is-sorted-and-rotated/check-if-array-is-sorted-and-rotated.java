3d9f4f7a08cbeb8177eef6550136203a1771c7de
Syed-Muntazir-Abbas <abbasmuntazir62@gmail.com>
2026-08-28 00:56:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (47.01%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -2,18 +2,17 @@ class Solution {
     public boolean check(int[] nums) {
         int count = 0;
         int n = nums.length;
-
-        for(int i = 0; i < n - 1; i++){
-            if(nums[i] > nums[i + 1]){
+        for(int i = 0;i < n-1;i++){
+            if(nums[i] > nums[i+1]){
                 count++;
             }
         }
+            if(nums[n-1] > nums[0]){
+                count++;
+            }
+            return count <= 1;
 
-        // circular check
-        if(nums[n - 1] > nums[0]){
-            count++;
-        }
-
-        return count <= 1;
-    }
+            }
 }
+        
+        
